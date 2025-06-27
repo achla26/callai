f9a00f3bcbcd4338fc07fc36da5360bb2b244031
achla dhiman <achlar8@gmail.com>
2025-06-27 16:51:31 +1200

    add drawer for mobile

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -7,15 +7,29 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger
-} from "@radix-ui/react-dropdown-menu";
+} from "@/components/ui/dropdown-menu";
+
+import {
+    Drawer,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger
+} from "@/components/ui/drawer";
 
 import { ChevronDown, CreditCard, LogOut } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
-
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
 const DashboardUserButton = () => {
     const router = useRouter();
+
+    const isMobile = useIsMobile();
+
     const { data, isPending } = authClient.useSession();
 
     const onLogOut = () => {
@@ -30,6 +44,75 @@ const DashboardUserButton = () => {
 
     if (isPending || !data?.user) return null;
 
+    if (isMobile) {
+        return (
+            <Drawer>
+                <DrawerTrigger>
+                    <button
+                        className={cn(
+                            "w-full h-15 px-3 flex items-center justify-between",
+                            "rounded-md hover:bg-primary ",
+                            "transition-colors focus-visible:outline-none focus-visible:ring-2",
+                            "focus-visible:ring-ring focus-visible:ring-offset-2",
+                            "ring-offset-background glass text-primary-foreground ",
+                        )}
+                    >
+                        <div className="flex items-center gap-2 overflow-hidden">
+                            <div className="size-10 flex-shrink-0 rounded-full bg-yellow-400 flex items-center justify-center text-xs font-bold uppercase">
+                                {(data.user.name?.slice(0, 2) || "UA")}
+                            </div>
+                            <div className="flex flex-col items-start overflow-hidden">
+                                <span className="text-sm font-medium truncate">
+                                    {data.user.name}
+                                </span>
+                                <span className="text-sm truncate">
+                                    {data.user.email}
+                                </span>
+                            </div>
+
+                        </div>
+                        <ChevronDown className="size-4 opacity-70" />
+                    </button>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>
+                            <div className="flex items-center gap-2 overflow-hidden">
+                                <div className="size-10 flex-shrink-0 rounded-full bg-yellow-400 flex items-center justify-center text-xs font-bold uppercase">
+                                    {(data.user.name?.slice(0, 2) || "UA")}
+                                </div>
+                                <div className="flex flex-col items-start overflow-hidden">
+                                    <span className="text-sm font-medium truncate">
+                                        {data.user.name}
+                                    </span>
+                                    <span className="text-sm text-gray-500 truncate">
+                                        {data.user.email}
+                                    </span>
+                                </div>
+                            </div>
+                        </DrawerTitle>
+                        {/* <DrawerDescription>{data.user.email}</DrawerDescription> */}
+                        <DrawerFooter>
+                            <Button
+                                variant="outline"
+                                onClick={onLogOut}
+                            >
+                                <CreditCard className="size-4" />
+                                Billing
+                            </Button>
+                            <Button
+                                variant="outline"
+                                onClick={onLogOut}
+                            >
+                                Logout
+                                <LogOut className="size-4 text-muted" />
+                            </Button>
+                        </DrawerFooter>
+                    </DrawerHeader>
+                </DrawerContent>
+            </Drawer>
+        )
+    }
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
